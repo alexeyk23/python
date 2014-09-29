1fcb73d8c66268407483891a09f9102907525b96
trebushet <kunak94@mail.ru>
2014-09-29 23:31:19 +0400

    redacted secondproblem

--- secondProblem/secondProblem.py
+++ secondProblem/secondProblem.py
@@ -1,28 +1,31 @@
+# coding=utf-8
 __author__ = 'trebushet'
+#в input.txt email адреса, где @ заменена на [sobaka],[at],[dog], а точка может быть
+#заменена на _ в output.txt выписать исправленные адреса. в fail.txt те, которые не исправили
+dogWords = ["[sobaka]", "[at]", "[dog]"]
 def fixedEmail(email):
-    email=email.strip(' \t\n\r')
     #replace all substring to @
-    email=email.replace("[sobaka]","@")
-    email=email.replace("[at]", "@")
-    email=email.replace("[dog]","@")
+    for dog in dogWords:
+        email = email.replace(dog, "@")
     #find last char '_'and replace on '.'
-    indLast=email.rfind("_");
-    if indLast!=-1 and indLast!=len(email):
-        email=email[:indLast]+"."+email[indLast+1:]
-    if email[0]!='@' and email.count("@")==1 and email[-1:].isalpha():
+    indLast = email.rfind("_")
+    if indLast != -1 and indLast != len(email):
+        email = email[:indLast] + "." + email[indLast + 1:]
+    if email[0] != '@' and email.count("@") == 1 and email[-1:].isalpha():
         return email
     else:
-        return "fail"
+        return None
 
-fInput = open("input.txt",'r')
-fOutput=open("output.txt",'w')
-fFail=open("failOutput.txt",'w')
-for line in fInput:
-    res=fixedEmail(line)
-    if res=="fail":
-        fFail.write(line+'\n')
+fInput = open("input.txt", 'r')
+fOutput = open("output.txt", 'w')
+fFail = open("failOutput.txt", 'w')
+words = fInput.read().split()
+for line in words:
+    res = fixedEmail(line)
+    if res is None:
+        fFail.write("{0}\n".format(line))
     else:
-        fOutput.write(res+'\n')
+        fOutput.write("{0}\n".format(res))
 fInput.close()
 fOutput.close()
 fFail.close()
